804d5158e08a1cea4765df0bbc13a97de41184f0
Sikati Kenmogne Samuel <sikatikenmogne@gmail.com>
2024-06-11 06:29:54 +0100

    test: add unit tests for CalculatorButton
    This commit adds unit tests for the CalculatorButton widget to ensure its functionality and behavior. The tests cover displaying the correct button name, calling the onPressed callback when pressed, verifying the text style, and checking the button color. These tests enhance the reliability and maintainability of the calculator application.

---
+++ test/calculator_button_test.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_calculator/src/widgets/calculator_button.dart';
+
+void main() {
+  testWidgets('CalculatorButton displays correct name',
+      (WidgetTester tester) async {
+    final buttonName = 'Test Button';
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          body: CalculatorButton(
+            name: buttonName,
+            onPressed: () {},
+          ),
+        ),
+      ),
+    );
+
+    final buttonFinder = find.text(buttonName);
+    expect(buttonFinder, findsOneWidget);
+  });
+
+  testWidgets('CalculatorButton calls onPressed when pressed',
+      (WidgetTester tester) async {
+    bool onPressedCalled = false;
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          body: CalculatorButton(
+            name: 'Test Button',
+            onPressed: () {
+              onPressedCalled = true;
+            },
+          ),
+        ),
+      ),
+    );
+
+    final buttonFinder = find.byType(ElevatedButton);
+    expect(buttonFinder, findsOneWidget);
+
+    await tester.tap(buttonFinder);
+    expect(onPressedCalled, true);
+  });
+
+  testWidgets('CalculatorButton has correct text style',
+      (WidgetTester tester) async {
+    final textStyle = TextStyle(fontSize: 20.0, color: Colors.red);
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          body: CalculatorButton(
+            name: 'Test Button',
+            onPressed: () {},
+            textStyle: textStyle,
+          ),
+        ),
+      ),
+    );
+
+    final textFinder = find.text('Test Button');
+    final textWidget = tester.widget<Text>(textFinder);
+    expect(textWidget.style, textStyle);
+  });
+
+  testWidgets('CalculatorButton has correct button color',
+      (WidgetTester tester) async {
+    final buttonColor = Colors.blue;
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          body: CalculatorButton(
+            name: 'Test Button',
+            onPressed: () {},
+            buttonColor: buttonColor,
+          ),
+        ),
+      ),
+    );
+
+    final buttonFinder = find.byType(ElevatedButton);
+    final buttonWidget = tester.widget<ElevatedButton>(buttonFinder);
+    expect(buttonWidget.style!.backgroundColor!.resolve({}), buttonColor);
+  });
+}
