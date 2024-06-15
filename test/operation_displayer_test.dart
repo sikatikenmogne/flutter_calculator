79888b6673b43d9696752473dedf688ab8223963
Sikati Kenmogne Samuel <sikatikenmogne@gmail.com>
2024-06-15 08:45:14 +0100

    test: add unit tests for OperationDisplayer
    This commit adds unit tests for the OperationDisplayer widget to ensure it displays the correct operation and style. The tests cover displaying the default operation, displaying a provided operation, displaying the default style, and displaying a provided style. These tests enhance the reliability and maintainability of the calculator application.

---
+++ test/operation_displayer_test.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_calculator/src/models/operation.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_calculator/src/models/calculator_operator_enum.dart';
+import 'package:flutter_calculator/src/widgets/operation_displayer.dart';
+
+void main() {
+  group('OperationDisplayer', () {
+    testWidgets('Should display the default operation',
+        (WidgetTester tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: OperationDisplayer(),
+        ),
+      );
+
+      expect(find.text('0'), findsOneWidget);
+    });
+
+    testWidgets('Should display the provided operation',
+        (WidgetTester tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: OperationDisplayer(
+            firstOperand: 5.0,
+            secondOperand: 3.0,
+            calculatorOperator: CalculatorOperator.add,
+            operationToDisplay: Operation(
+              firstOperand: 5.0,
+              secondOperand: 3.0,
+              calculatorOperator: CalculatorOperator.add,
+              operationEnded: true,
+            ),
+          ),
+        ),
+      );
+
+      expect(find.text('5 + 3 ='), findsOneWidget);
+    });
+
+    testWidgets('Should display the default style',
+        (WidgetTester tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: OperationDisplayer(),
+        ),
+      );
+
+      final textWidget = tester.widget<Text>(find.byType(Text));
+      expect(textWidget.style?.fontSize, equals(20.0));
+      expect(textWidget.style?.color, equals(Colors.grey));
+    });
+
+    testWidgets('Should display the provided style',
+        (WidgetTester tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: OperationDisplayer(
+            fontSize: 24.0,
+            color: Colors.blue,
+          ),
+        ),
+      );
+
+      final textWidget = tester.widget<Text>(find.byType(Text));
+      expect(textWidget.style?.fontSize, equals(24.0));
+      expect(textWidget.style?.color, equals(Colors.blue));
+    });
+
+    // Add more test cases as needed
+  });
+}
