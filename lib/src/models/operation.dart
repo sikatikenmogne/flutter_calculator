4dc21095e1e835d63d846941487a83b1fa545222
Sikati Kenmogne Samuel <sikatikenmogne@gmail.com>
2024-06-21 06:37:40 +0100

    docs: add comments in `Operation` class for readability

--- lib/src/models/operation.dart
+++ lib/src/models/operation.dart
@@ -1,5 +1,6 @@
 import 'operator.dart';
 
+/// Represents a mathematical operation.
 class Operation {
   final double firstOperand;
   final double secondOperand;
@@ -18,18 +19,17 @@ class Operation {
         : _formatNumber(firstOperand);
   }
 
-  const Operation(
-      {this.firstOperand = 0,
-      this.secondOperand = 0,
-      this.calculatorOperator,
-      this.operationEnded = false});
-
+  /// Returns whether the operation is a binary operation.
   bool get isBinaryOperation {
     return calculatorOperator != Operator.none && calculatorOperator != null
         ? calculatorOperator!.isBinaryOperator
         : false;
   }
 
+  /// Returns whether the operation is complete.
+  ///
+  /// An operation is considered complete if the [calculatorOperator] is not
+  /// [Operator.none] and the operands are set accordingly.
   bool get isComplete {
     if (calculatorOperator != Operator.none) {
       if (isBinaryOperation) {
@@ -42,12 +42,22 @@ class Operation {
     }
   }
 
+  /// Computes the result of the operation.
+  ///
+  /// If the operation is a binary operation, it performs the operation using
+  /// the [calculatorOperator] on the [firstOperand] and [secondOperand].
+  /// Otherwise, it performs the operation using the [calculatorOperator] on
+  /// the [firstOperand] and null.
   num compute() {
     return (isBinaryOperation)
         ? calculatorOperator!.operation(firstOperand, secondOperand)
         : calculatorOperator!.operation(firstOperand, null);
   }
 
+  /// Formats a number as a string.
+  ///
+  /// If the number is an integer, it returns the integer value as a string.
+  /// Otherwise, it returns the number as a string.
   String _formatNumber(double number) {
     if (number % 1 == 0) {
       return number.toInt().toString();
@@ -56,16 +66,26 @@ class Operation {
     }
   }
 
+  /// Renders a binary operation as a string.
+  ///
+  /// The string representation includes the [firstOperand], [calculatorOperator],
+  /// [secondOperand], and [operationEnded] flag.
   String _renderBinaryOperation(double firstOperand, double secondOperand,
       Operator? calculatorOperator, bool operationEnded) {
     return "${_formatNumber(firstOperand)}${calculatorOperator != Operator.none ? " ${calculatorOperator}" : ""}${secondOperand != 0 && operationEnded ? " ${_formatNumber(secondOperand)}" : ""}${operationEnded ? " =" : ""}";
   }
 
+  /// Renders a unary operation as a string.
+  ///
+  /// The string representation includes the [uniqueOperand], [calculatorOperator],
+  /// and [operationEnded] flag.
   String _renderUnaryOperation(
       double uniqueOperand, Operator? calculatorOperator, bool operationEnded) {
     return "${calculatorOperator}(${_formatNumber(uniqueOperand)})${operationEnded ? " =" : ""}";
   }
 
+  /// Creates a copy of the [Operation] instance with the specified properties
+  /// replaced with new values.
   Operation copyWith({
     double? firstOperand,
     double? secondOperand,
@@ -80,6 +100,10 @@ class Operation {
     );
   }
 
+  /// Clears the values of the given [currentOperation].
+  ///
+  /// Sets the first and second operands to 0, the calculator operator to none,
+  /// and operationEnded to false.
   static void clear(Operation currentOperation) {
     currentOperation.firstOperand = 0;
     currentOperation.secondOperand = 0;
