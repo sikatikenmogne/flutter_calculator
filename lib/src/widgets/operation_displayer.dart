3bd2373ccb94a78d5604abb11eae0b4ea0925b6b
Sikati Kenmogne Samuel <sikatikenmogne@gmail.com>
2024-06-21 06:48:13 +0100

    refactor: update OperationDisplayer widget to handle nullable operation

--- lib/src/widgets/operation_displayer.dart
+++ lib/src/widgets/operation_displayer.dart
@@ -1,16 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_calculator/src/models/calculator_operator_enum.dart';
 
 import '../models/operation.dart';
 
 class OperationDisplayer extends StatelessWidget {
-  final Operation operationToDisplay;
+  final Operation? operationToDisplay;
   final double padding;
   final double fontSize;
   final Color color;
   OperationDisplayer(
       {Key? key,
-      this.operationToDisplay = const Operation(),
+      this.operationToDisplay,
       this.padding = 8.0,
       this.fontSize = 20.0,
       this.color = Colors.grey})
@@ -21,7 +20,7 @@ class OperationDisplayer extends StatelessWidget {
     return Padding(
       padding: EdgeInsets.fromLTRB(padding, padding, padding, 0.0),
       child: Text(
-        operationToDisplay.toString(),
+        operationToDisplay?.toString() ?? Operation().toString(),
         style: TextStyle(fontSize: fontSize, color: color),
         textAlign: TextAlign.end,
       ),
